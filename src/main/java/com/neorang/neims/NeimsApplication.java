500cfe5c08d53a820ba17cdbb17a4619434ef611
clemado1 <clemado1@gmail.com>
2022-08-05 17:05:31 +0900

    first commit

---
+++ src/main/java/com/neorang/neims/NeimsApplication.java
@@ -0,0 +1,13 @@
+package com.neorang.neims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neorang/neims/NeimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neorang.neims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
