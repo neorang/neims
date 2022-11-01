a06b1daeaa33b7df85fb41c6c0345922bf59a075
clemado1 <clemado1@gmail.com>
2022-11-01 23:07:01 +0900

    front: add: build path spring resources 지정 및 proxy 설정

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // build path
+  outputDir: "../src/main/resources/static",
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8085',
+        changeOrigin: true
+      }
+    }
+  }
 })
